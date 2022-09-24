0d60f7b23e31da45c197a6091e7d138c04d0e349
HarmanBoparai <erharmanboparai@gmail.com>
2022-09-23 21:46:39 -0400

    created function to check the time of an event and compared it

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 
-
+//Variable for displaying time
 var timeDisplayEl = $('#time-display');
 
 // function for displaying current date and time
@@ -25,3 +25,35 @@ $('input[type="text"]').each(function () {
     const letsGrab = localStorage.getItem(getting); 
     document.getElementById(getting).value = letsGrab; 
 });
+
+var currentHour = moment().format("H"); // variable for getting the value of time in current hour
+
+// colorcode for different timeblocks of the day
+colorCode(9,  "#entertext1");  //colorcode(hour,id)
+colorCode(10, "#entertext2"); 
+colorCode(11, "#entertext3"); 
+colorCode(12, "#entertext4"); 
+colorCode(13, "#entertext5");
+colorCode(14, "#entertext6"); 
+colorCode(15, "#entertext7"); 
+colorCode(16, "#entertext8"); 
+colorCode(17, "#entertext9"); 
+colorCode(18, "#entertext10");
+colorCode(19, "#entertext11");
+colorCode(20, "#entertext12");
+colorCode(21, "#entertext13");
+colorCode(22, "#entertext14");
+
+//function to check the time of an event and compare it with the current time
+function colorCode(time, theId) { 
+    if (currentHour> time) { 
+        $(theId).addClass("past"); 
+    }
+    if (currentHour < time) { 
+        $(theId).addClass("future"); 
+    }
+    if (currentHour == time) { 
+        $(theId).addClass("present");
+    }
+   console.log('time: ' , time , 'theID: ' , theId);
+}
