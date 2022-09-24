3dec99ea8fee7cf20771cf2d75f4c56c8569f640
HarmanBoparai <erharmanboparai@gmail.com>
2022-09-23 20:54:18 -0400

    stored and retrieved  the events from the local storage

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,19 @@ function displayTime() {
   }
   setInterval(displayTime, 1000);
 console.log(moment().format()); //console log it to see it
+
+//Store the values added to the events in the local storage
+$(".saveBtn").click(function () { 
+    $('input[type="text"]').each(function () { 
+        const key = $(this).attr('id'); 
+        const value = $(this).val(); 
+        localStorage.setItem(key, value);
+    });
+});
+
+//Getting the stored values of the events from the local storage to display 
+$('input[type="text"]').each(function () { 
+    const getting = $(this).attr('id'); 
+    const letsGrab = localStorage.getItem(getting); 
+    document.getElementById(getting).value = letsGrab; 
+});
